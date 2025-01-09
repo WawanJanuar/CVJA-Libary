b4a72d476f689f531ea20a16874c5adadfc7b2ff
WawanJanuar <wawanjanuar547@gmail.com>
2025-01-09 18:44:30 +0700

    ubah routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Regis from './Registrasi/Regis'
+import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Regis />
+    <App />
   </React.StrictMode>
 );
